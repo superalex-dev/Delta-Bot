915468e9cf6cc309052931a81fdccaf8958e8e67
Alexander Boev <naidobrixxjr@gmail.com>
2022-11-21 12:26:53 +0200

    Initial commit

---
+++ DeltaBot/DeltaBot/DeltaBot/Modules/Commands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DeltaBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong!");
+        }
+
+        [Command("hello")]
+        public async Task Hello()
+        {
+            await ReplyAsync("Hello!");
+        }
+
+        [Command("chukai")]
+
+        public async Task Chukai()
+        {
+            await ReplyAsync("CHUKAI KURVATA BRATLE!");
+        }
+    }
+}
